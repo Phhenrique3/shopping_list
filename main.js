30b044a3b7950a1c60d669416001db5fde2aa998
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-02-04 21:44:41 -0300

    refactor: melhora a estrutura do código

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const ulitens = document.getElementById("lista-de-itens");
 form.addEventListener("submit", function (evento) {
   evento.preventDefault(); // Previne o comportamento padrão de envio do formulário
   salvarItem(); // Chama a função para salvar o item
-  mostraItem();
+  mostraItem()
 });
 
 // Função responsável por salvar um novo item na lista de compras
@@ -36,19 +36,23 @@ function salvarItem() {
   console.log(listaDeItens); // Exibe o conteúdo atualizado de listaDeItens no console
 }
 
-function mostraItem() {
-  ulitens.innerHTML = "";
-  listaDeItens.forEach((elemento, index) => {
+
+function mostraItem(){
+    ulitens.innerHTML = "";
+    listaDeItens.forEach((elemento,index) =>{
+
     ulitens.innerHTML += `   
     
     <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
         <div>
             <input type="checkbox" class="is-clickable" />
-            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+            <input type="text" class="is-size-5" style="color: red;" value="${elemento.valor}"></input>
         </div>
         <div>
             <i class="fa-solid fa-trash is-clickable deletar"></i>
         </div>
-    </li>`;
-  });
+    </li>`
+
+    })
+
 }
