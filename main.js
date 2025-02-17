306dcbb962d8b1deeaa8ac1a019c9dc820457c87
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-02-17 20:55:15 -0300

    refactor: implementa funcionalidade de exclusão e adiciona informação sobre itens comprados

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 let listaDeItens = [];
 
+let itemAEditar;
+
 // Referência ao formulário HTML com o ID "form-itens"
 const form = document.getElementById("form-itens");
 
 // Referência ao campo de entrada de texto HTML com o ID "receber-item"
 const itensInput = document.getElementById("receber-item");
 
+// Referência ao elemento <ul> HTML com o ID "lista-de-itens"
 const ulitens = document.getElementById("lista-de-itens");
+
+// Referência ao elemento <ul> HTML com o ID "itens-comprados"
+const ulItensComprados = document.getElementById("itens-comprados");
 
 // Adiciona um ouvinte de evento ao formulário que intercepta o evento de envio (submit)
 form.addEventListener("submit", function (evento) {
   evento.preventDefault(); // Previne o comportamento padrão de envio do formulário
   salvarItem(); // Chama a função para salvar o item
-  mostraItem();
+  mostraItem(); // Chama a função para exibir os itens atualizados
 });
 
 // Função responsável por salvar um novo item na lista de compras
 function salvarItem() {
   const compraItem = itensInput.value; // Obtém o valor do item a partir do campo de entrada
+
   // Verifica se o item já existe na lista comparando os valores em maiúsculas
   const ValidarIten = listaDeItens.find(
     (elemento) => elemento.valor.toUpperCase() === compraItem.toUpperCase()
@@ -33,26 +40,46 @@ function salvarItem() {
       checar: false,
     });
   }
+
   itensInput.value = ""; // Limpa o campo de entrada
   console.log(listaDeItens); // Exibe o conteúdo atualizado de listaDeItens no console
 }
 
+// Função responsável por exibir os itens da lista de compras na interface do usuário
 function mostraItem() {
-  ulitens.innerHTML = ""; // Limpa o conteúdo atual do elemento <ul>
+  ulitens.innerHTML = ""; // Limpa o conteúdo atual do elemento <ul> "lista-de-itens"
+  ulItensComprados.innerHTML = ""; // Limpa o conteúdo atual do elemento <ul> "itens-comprados"
 
   // Itera sobre cada item na lista de itens
   listaDeItens.forEach((elemento, index) => {
-    // Adiciona um novo item <li> ao elemento <ul> com os detalhes do item de compra
-    ulitens.innerHTML += `   
-    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
-        <div>
+    if (elemento.checar) {
+      // Adiciona um novo item <li> ao elemento <ul> "itens-comprados" com os detalhes do item de compra
+      ulItensComprados.innerHTML += `
+        <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+          <div>
+            <input type="checkbox" checked class="is-clickable" />
+            <input type="text" class="is-size-5" style="text-decoration: line-through;" value="${elemento.valor}"></input>
+          </div>
+          <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+          </div>
+        </li>
+      `;
+    } else {
+      // Adiciona um novo item <li> ao elemento <ul> "lista-de-itens" com os detalhes do item de compra
+      ulitens.innerHTML += `
+        <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+          <div>
             <input type="checkbox" class="is-clickable" />
             <input type="text" class="is-size-5" style="color: red;" value="${elemento.valor}"></input>
-        </div>
-        <div>
+          </div>
+          <div>
+          <i class="fa-regular fa-floppy-disk is-clickable"></i><i class="fa-regular is-clickable fa-pen-to-square editar"></i>
             <i class="fa-solid fa-trash is-clickable deletar"></i>
-        </div>
-    </li>`;
+          </div>
+        </li>
+      `;
+    }
   });
 
   // Seleciona todos os checkboxes dos itens de compra
@@ -60,11 +87,42 @@ function mostraItem() {
 
   // Adiciona um ouvinte de evento de clique a cada checkbox
   inuptCheck.forEach((i) => {
-    i.addEventListener('click', (evento) => {
+    i.addEventListener("click", (evento) => {
+      const valorDoElemento =
+        evento.target.parentElement.parentElement.getAttribute("data-value");
+      listaDeItens[valorDoElemento].checar = evento.target.checked; // Atualiza a propriedade "checar" do item correspondente
+      console.log(listaDeItens[valorDoElemento].checar); // Exibe o estado atualizado do item no console
+      mostraItem(); // Atualiza a lista exibida
+    });
+  });
+
+  const deletarObjetos = document.querySelectorAll(".deletar");
+
+  deletarObjetos.forEach((i) => {
+    i.addEventListener("click", (evento) => {
       const valorDoElemento =
         evento.target.parentElement.parentElement.getAttribute("data-value");
       listaDeItens[valorDoElemento].checar = evento.target.checked;
-     console.log(listaDeItens[valorDoElemento].checar)
+      listaDeItens.splice(valorDoElemento, 1);
+      mostraItem();
     });
   });
+
+  const editarItens = document.querySelectorAll(".editar");
+
+  editarItens.forEach((i) => {
+    i.addEventListener("click", (evento) => {
+      itemAEditar =
+        evento.target.parentElement.parentElement.getAttribute("data-value");
+      mostraItem()
+      console.log(itemAEditar);
+    });
+  });
+}
+
+
+
+function salvarEdicao(){
+  const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`)
+  console.log(itemEditado.value);
 }
