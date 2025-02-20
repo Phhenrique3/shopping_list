a8b04c81f450c7a8611e365491aa6d809e6a4606
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-02-19 22:42:19 -0300

    fix: correçao de bug

--- main.js
+++ main.js
@@ -70,10 +70,16 @@ function mostraItem() {
         <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
           <div>
             <input type="checkbox" class="is-clickable" />
-            <input type="text" class="is-size-5" style="color: red;" value="${elemento.valor}"${index != itemAEditar ? 'disabled': '' } ></input>
+            <input type="text" class="is-size-5" style="color: red;" value="${
+              elemento.valor
+            }"${index !== Number(itemAEditar) ? "disabled" : ""} ></input>
           </div>
           <div>
-          ${ index == itemAEditar ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>' : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
+          ${
+            index === Number(itemAEditar)
+              ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>'
+              : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'
+          }
             <i class="fa-solid fa-trash is-clickable deletar"></i>
           </div>
         </li>
@@ -124,7 +130,7 @@ function salvarEdicao() {
     `[data-value="${itemAEditar}"] input[type="text"]`
   );
   listaDeItens[itemAEditar].valor = itemEditado.value;
-  console.log(listaDeItens)
+  console.log(listaDeItens);
   itemAEditar = -1;
   mostraItem();
 }
